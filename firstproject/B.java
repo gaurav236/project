03c7570d2dec22a2ee24b663a4c1efc7e89fb4d2
gaurav <gauravuike236@gmail.com>
2022-04-19 15:55:18 +0530

    this is third

--- firstproject/B.java
+++ firstproject/B.java
@@ -7,6 +7,7 @@ System.out.println("your story might not have such a happy");
 System.out.println("Beginnig but that does not make you");
 System.out.println("Who you are , it is the rest of it--");
 System.out.println("WHO YOU CHOOSE TO BE");
+System.out.println("all mighty push");
 }
 
 
