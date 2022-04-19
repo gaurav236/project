fd2643a3156b12f5e1fdddae93ad24d36eccfa9e
gaurav236 <103994605+gaurav236@users.noreply.github.com>
2022-04-19 15:35:27 +0530

    Add files via upload

---
+++ firstproject/A.java
@@ -0,0 +1,18 @@
+public class A
+{
+public static void main(String args[])
+{
+System.out.println("NEVER GIVE UP");
+System.out.println("KNOW MORE THAN OTHERS");
+System.out.println("WORK MORE THAN OTHERS");
+System.out.println("EXPECT LESS THAN OTHERS");
+
+}
+
+
+
+
+}
+
+
+
---
+++ firstproject/B.java
@@ -0,0 +1,19 @@
+public class B
+{
+
+public static void main(String args[])
+{
+System.out.println("your story might not have such a happy");
+System.out.println("Beginnig but that does not make you");
+System.out.println("Who you are , it is the rest of it--");
+System.out.println("WHO YOU CHOOSE TO BE");
+}
+
+
+
+
+
+
+
+
+}
